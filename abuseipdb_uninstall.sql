28186c8ddd2006fae7854dbe8e27f263fbd931b5
marcopolo <marcdecaria@me.com>
2023-05-27 09:31:20 -0400

    Update abuseipdb_uninstall.sql

--- abuseipdb_uninstall.sql
+++ abuseipdb_uninstall.sql
@@ -1,8 +1,14 @@
 -- Remove the configuration settings for the AbuseIPDB module
-DELETE FROM " . TABLE_CONFIGURATION . " WHERE configuration_key LIKE 'ABUSEIPDB_%';
+DELETE FROM configuration WHERE configuration_key LIKE 'ABUSEIPDB_%';
 
 -- Remove the configuration group for the AbuseIPDB module
-DELETE FROM " . TABLE_CONFIGURATION_GROUP . " WHERE configuration_group_title = 'AbuseIPDB Configuration';
+DELETE FROM configuration_group WHERE configuration_group_title = 'AbuseIPDB Configuration';
 
 -- Remove the admin page for the AbuseIPDB module
-DELETE FROM " . TABLE_ADMIN_PAGES . " WHERE page_key = 'configAbuseIPDB';
+DELETE FROM admin_pages WHERE page_key = 'configAbuseIPDB';
+
+-- Drop the abuseipdb_cache table
+DROP TABLE IF EXISTS abuseipdb_cache;
+
+-- Drop the abuseipdb_maintenance table
+DROP TABLE IF EXISTS abuseipdb_maintenance;
