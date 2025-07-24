bb24c58230ccf7e68040d16732f1aae4e260df09
marcopolo <marcdecaria@me.com>
2025-07-24 19:32:42 -0400

    v4.0.8

--- zc_plugins/AbuseIPDB/v4.0.8/Installer/ScriptedInstaller.php
+++ zc_plugins/AbuseIPDB/v4.0.8/Installer/ScriptedInstaller.php
@@ -36,16 +36,16 @@ class ScriptedInstaller extends ScriptedInstallBase
 
             // Fallback to define table constants if not already defined
             if (!defined('TABLE_ABUSEIPDB_CACHE')) {
-                define('TABLE_ABUSEIPDB_CACHE', 'abuseipdb_cache');
+                define('TABLE_ABUSEIPDB_CACHE', DB_PREFIX . 'abuseipdb_cache');
             }
             if (!defined('TABLE_ABUSEIPDB_MAINTENANCE')) {
-                define('TABLE_ABUSEIPDB_MAINTENANCE', 'abuseipdb_maintenance');
+                define('TABLE_ABUSEIPDB_MAINTENANCE', DB_PREFIX . 'abuseipdb_maintenance');
             }
             if (!defined('TABLE_ABUSEIPDB_FLOOD')) {
-                define('TABLE_ABUSEIPDB_FLOOD', 'abuseipdb_flood');
+                define('TABLE_ABUSEIPDB_FLOOD', DB_PREFIX . 'abuseipdb_flood');
             }
             if (!defined('TABLE_ABUSEIPDB_ACTIONS')) {
-                define('TABLE_ABUSEIPDB_ACTIONS', 'abuseipdb_actions');
+                define('TABLE_ABUSEIPDB_ACTIONS', DB_PREFIX . 'abuseipdb_actions');
             }
 
             // Create or get configuration group ID
@@ -288,16 +288,16 @@ class ScriptedInstaller extends ScriptedInstallBase
 
             // Fallback to define table constants if not already defined
             if (!defined('TABLE_ABUSEIPDB_CACHE')) {
-                define('TABLE_ABUSEIPDB_CACHE', 'abuseipdb_cache');
+                define('TABLE_ABUSEIPDB_CACHE', DB_PREFIX . 'abuseipdb_cache');
             }
             if (!defined('TABLE_ABUSEIPDB_MAINTENANCE')) {
-                define('TABLE_ABUSEIPDB_MAINTENANCE', 'abuseipdb_maintenance');
+                define('TABLE_ABUSEIPDB_MAINTENANCE', DB_PREFIX . 'abuseipdb_maintenance');
             }
             if (!defined('TABLE_ABUSEIPDB_FLOOD')) {
-                define('TABLE_ABUSEIPDB_FLOOD', 'abuseipdb_flood');
+                define('TABLE_ABUSEIPDB_FLOOD', DB_PREFIX . 'abuseipdb_flood');
             }
             if (!defined('TABLE_ABUSEIPDB_ACTIONS')) {
-                define('TABLE_ABUSEIPDB_ACTIONS', 'abuseipdb_actions');
+                define('TABLE_ABUSEIPDB_ACTIONS', DB_PREFIX . 'abuseipdb_actions');
             }
 
             // Get configuration group ID
